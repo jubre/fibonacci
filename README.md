70f273923edc1d78448bac1b119a4aef89f8c093
Junior Corman <junior.corman@gmail.com>
2018-03-29 13:16:15 -0500

    First version. work but, need to optimize

---
+++ README.md
@@ -0,0 +1,2 @@
+#Fibonacci
+Calcula los dos ultimos numeros de fibonacci, dado un numero de fibonacci.
---
+++ fibo.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Funcion que calcula el numero de Fibonacci
+def fibonacci(numero):
+	if (numero < 2):
+		return numero
+	else:
+		return fibonacci(numero-1)+fibonacci(numero-2)
+
+# Funcion que valida si el numero ingreso es
+def validar(iterador, input):
+    if(input == fibonacci(iterador)):
+        return iterador
+    else:
+        iterador += 1
+        return validar(iterador, input)
+
+# Funcion principal
+def run():
+    input = int(raw_input('Escribe el numero Fibonacci: '))
+
+    posicion = validar(0, input)
+
+    print 'Fibonacci(' + str(input) + ') -> ' + str(fibonacci(posicion-1)) + ', ' + str(fibonacci(posicion-2))
+
+# Nuestro programa empieza aqui.
+if __name__ == '__main__':
+	run()
