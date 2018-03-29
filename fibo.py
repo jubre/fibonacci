174ff4b4d1771ba86208e1f01828e4333567fcc7
Junior Corman <junior.corman@gmail.com>
2018-03-29 17:40:27 -0500

    update coment run

--- fibo.py
+++ fibo.py
@@ -10,18 +10,18 @@ def fibonacci(numero):
 # Funcion que valida si el numero ingreso coincide con un numero de Fibonacci
 def validar(iterador, input):
 	if(input == fibonacci(iterador)):
-        return iterador
-    else:
-        iterador += 1
-        return validar(iterador, input)
+		return iterador
+	else:
+		iterador += 1
+		return validar(iterador, input)
 
-# Funcion principal
+# Funcion principal.
 def run():
-    input = int(raw_input('Escribe el numero Fibonacci: '))
+	input = int(raw_input('Escribe el numero Fibonacci: '))
 
-    posicion = validar(0, input)
+	posicion = validar(0, input)
 
-    print 'Fibonacci(' + str(input) + ') -> ' + str(fibonacci(posicion-1)) + ', ' + str(fibonacci(posicion-2))
+	print 'Fibonacci(' + str(input) + ') -> ' + str(fibonacci(posicion-1)) + ', ' + str(fibonacci(posicion-2))
 
 # Nuestro programa empieza aqui.
 if __name__ == '__main__':
